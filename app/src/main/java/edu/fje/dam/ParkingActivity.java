ab1c372677e151ad6e6d9a16c6512cfef9f712ff
Ivan <ivanraid@gmail.com>
2020-04-15 21:15:57 +0200

    el parking recibe los datos del main y los muestra

--- app/src/main/java/edu/fje/dam/ParkingActivity.java
+++ app/src/main/java/edu/fje/dam/ParkingActivity.java
@@ -2,6 +2,7 @@ package edu.fje.dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +19,28 @@ public class ParkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking);
+
+        //coger los mensajes del intent
+        Intent intent = getIntent();
+        String nom = intent.getStringExtra(MainActivity.NOM_PARKING);
+        String quantitatCoches = intent.getStringExtra(MainActivity.QUANTITAT_COCHES);
+        String quantitatMotos = intent.getStringExtra(MainActivity.QUANTITAT__MOTOS);
+
+        //poner los mensajes del intent
+        TextView textNom = findViewById(R.id.textNomParking);
+        TextView textCoche = findViewById(R.id.textNumCoche);
+        TextView textMoto = findViewById(R.id.textNumMoto);
+        textNom.setText(nom);
+        textCoche.setText(quantitatCoches);
+        textMoto.setText(quantitatMotos);
+
+
     }
 
     public void entrarCoche(View v){
-        final Button sumCoche = (Button) findViewById(R.id.sumCoche);
-        final Button resCoche = (Button) findViewById(R.id.restCoche);
-        final TextView textCoche = (TextView) findViewById(R.id.textNumCoche);
+        final Button sumCoche = findViewById(R.id.sumCoche);
+        final Button resCoche = findViewById(R.id.restCoche);
+        final TextView textCoche = findViewById(R.id.textNumCoche);
         parkingCoche.anadirCoche(new Coche());
         textCoche.setText(marcadorCoche.getNumeroPlazasLibres()+"");
         if(marcadorCoche.getNumeroPlazasLibres() < marcadorCoche.getNumeroPlazas()){

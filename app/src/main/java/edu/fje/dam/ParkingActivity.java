60446869879b46e3c6b3bff8a7ea2ef31f775363
Jordi Velasco <030177.clot@fje.edu>
2020-04-15 23:46:54 +0200

    funcionalitat moto implementada

--- app/src/main/java/edu/fje/dam/ParkingActivity.java
+++ app/src/main/java/edu/fje/dam/ParkingActivity.java
@@ -30,27 +30,66 @@ public class ParkingActivity extends AppCompatActivity {
         TextView textNom = findViewById(R.id.textNomParking);
         TextView textCoche = findViewById(R.id.textNumCoche);
         TextView textMoto = findViewById(R.id.textNumMoto);
+        Button restCoche = findViewById(R.id.restCoche);
+        Button restMoto = findViewById(R.id.restMoto);
         textNom.setText(nom);
         textCoche.setText(quantitatCoches);
         textMoto.setText(quantitatMotos);
+        int numero = Integer.parseInt(quantitatCoches);
+        marcadorCoche.setNumeroPlazas(numero);
+        parkingCoche.anadirObserverCoche(marcadorCoche);
+        textCoche.setText(marcadorCoche.getNumeroPlazasLibres()+"");
+        marcadorMoto.setNumeroPlazas(numero);
+        parkingMoto.anadirObserverCoche(marcadorMoto);
+        textMoto.setText(marcadorMoto.getNumeroPlazasLibres()+"");
+        //circleMotos.setFill(Paint.valueOf("#2bff00")); <--- pone el semaforo en verde, por si usamos algo de eso
+
+        restCoche.setEnabled(false);
+        restMoto.setEnabled(false);
 
 
     }
 
     public void entrarCoche(View v){
-        final Button sumCoche = findViewById(R.id.sumCoche);
-        final Button resCoche = findViewById(R.id.restCoche);
-        final TextView textCoche = findViewById(R.id.textNumCoche);
+        Button sumCoche = findViewById(R.id.sumCoche);
+        Button resCoche = findViewById(R.id.restCoche);
+        TextView textCoche = findViewById(R.id.textNumCoche);
+
         parkingCoche.anadirCoche(new Coche());
         textCoche.setText(marcadorCoche.getNumeroPlazasLibres()+"");
         if(marcadorCoche.getNumeroPlazasLibres() < marcadorCoche.getNumeroPlazas()){
-            resCoche.setEnabled(false);
+            resCoche.setEnabled(true);
         }
         if(marcadorCoche.getNumeroPlazasLibres() <= 0){
-            sumCoche.setEnabled(true);
+            //circleCoche.setFill(Paint.valueOf("Red"));
+            sumCoche.setEnabled(false);
 
         }else {
-            textCoche.setText("hola");
+            //circleCoche.setFill(Paint.valueOf("#2bff00"));
         }
+    }
+
+    public void salirCoche(View v){
+        Button sumCoche = findViewById(R.id.sumCoche);
+        Button resCoche = findViewById(R.id.restCoche);
+        TextView textCoche = findViewById(R.id.textNumCoche);
+
+        parkingCoche.salirCoche(0);
+        textCoche.setText(marcadorCoche.getNumeroPlazasLibres()+"");
+        if(marcadorCoche.getNumeroPlazasLibres() < marcadorCoche.getNumeroPlazas()){
+            resCoche.setEnabled(true);
+        }else if(marcadorCoche.getNumeroPlazasLibres() == marcadorCoche.getNumeroPlazas()){
+            resCoche.setEnabled(false);
+        }
+        if(marcadorCoche.getNumeroPlazasLibres() >= 0){
+            //circleCoche.setFill(Paint.valueOf("#2bff00"));
+            sumCoche.setEnabled(true);
+
+        }
+
+        else {
+            //circleCoche.setFill(Paint.valueOf("#Red"));
+        }
+
     }
 }
